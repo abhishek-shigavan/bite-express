53968a6027e97c6f8752b6056f637a57642cce1c
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-07 20:18:26 +0530

    ADD: static url added

---
+++ src/utils/config.js
@@ -0,0 +1,2 @@
+export const HEADER_LOGO = "https://i.postimg.cc/sf4HHdtK/Screenshot-from-2023-08-05-19-39-58.png"
+export const RES_CARD_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"

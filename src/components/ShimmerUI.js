b1d31bc1efac50786f809f0e145d6c8ee0555170
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-08 12:12:22 +0530

    ADD: component created for Shimmer UI

---
+++ src/components/ShimmerUI.js
@@ -0,0 +1,15 @@
+const ShimmerUI = () => {
+  return (
+    <div className="shimmer-cnt">
+      <div className="shimmer-load-cnt"></div>
+      <div className="shimmer-load-cnt"></div>
+      <div className="shimmer-load-cnt"></div>
+      <div className="shimmer-load-cnt"></div>
+      <div className="shimmer-load-cnt"></div>
+      <div className="shimmer-load-cnt"></div>
+      <div className="shimmer-load-cnt"></div>
+    </div>
+  );
+};
+
+export default ShimmerUI;

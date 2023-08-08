07c86f8c347b80379e0105432778d92aff54f93a
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-08 12:13:26 +0530

    MODIFIED: fetching restaurant data from live API & added search bar

--- src/components/RestaurantCardContainer.js
+++ src/components/RestaurantCardContainer.js
@@ -1,11 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
 import mockData from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ShimmerUI from "./ShimmerUI";
 
 const RestaurantCardContainer = () => {
-  const [resData, setResData] = useState(mockData);
+  const [resData, setResData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchVal, setSearchVal] = useState("");
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const resJSON = await res.json();
+    setResData(
+      resJSON?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilteredData(
+      resJSON?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  return resData.length === 0 ? (
+    <ShimmerUI />
+  ) : (
     <div id="resCardMainCnt" className="res-card-main-cnt">
       <button
         id="topRateFilter"
@@ -15,6 +39,23 @@ const RestaurantCardContainer = () => {
         }}
       >
         Top Rated Restaurants
+      </button>
+      <input
+        type="text"
+        value={searchVal}
+        onChange={(e) => {
+          setSearchVal(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          const filteredList = filteredData.filter((res) =>
+            res.info.name.toLowerCase().includes(searchVal.toLowerCase())
+          );
+          setResData(filteredList);
+        }}
+      >
+        Search
       </button>
       <div id="resCardCnt" className="res-card-cnt">
         {resData.map((res) => {

9fbbf551ffd768c0e343a71fda5620f32b095c5d
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-09 12:04:31 +0530

    MODIFIED: routes added for home, offer & cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HeaderComponent from "./components/HeaderComponent";
 import RestaurantCardContainer from "./components/RestaurantCardContainer";
+import Offers from "./components/Offers";
+import Cart from "./components/Cart";
 
 const AppLayout = () => {
   return (
@@ -12,5 +15,11 @@ const AppLayout = () => {
   );
 };
 
+const AppRoutes = createBrowserRouter([
+  { path: "/", element: <AppLayout /> },
+  { path: "/offers", element: <Offers /> },
+  { path: "/cart", element: <Cart /> },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={AppRoutes} />);

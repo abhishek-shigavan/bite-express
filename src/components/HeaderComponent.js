b0140a36e8def359b4bd7a376888c9467eea4194
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-09 12:51:12 +0530

    MODIFIED: added routes for nav items

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,25 +1,26 @@
+import { Link } from "react-router-dom";
 import { HEADER_LOGO } from "../utils/config";
 
 const HeaderComponent = () => {
   return (
     <div id="headerCnt" className="header-cnt">
       <div id="logo" className="header-logo">
-        <img
-          className="header-logo-img"
-          src={HEADER_LOGO}
-          alt="Bite Express"
-        />
+        <img className="header-logo-img" src={HEADER_LOGO} alt="Bite Express" />
       </div>
       <div id="headerOptions" className="header-options">
         <ul>
           <li>
-            <h1>Home</h1>
+            <h1><Link className="route-link" to={"/"}>Home</Link></h1>
           </li>
           <li>
-            <h1>Offers</h1>
+            <h1>
+              <Link className="route-link" to={"/offers"}>Offers</Link>
+            </h1>
           </li>
           <li>
-            <h1>Cart</h1>
+            <h1>
+              <Link className="route-link" to={"/cart"}>Cart</Link>
+            </h1>
           </li>
           <li>
             <h1>Account</h1>

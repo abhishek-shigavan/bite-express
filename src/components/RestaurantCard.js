b550018addbcf9040e302bea160324f7ee03bd8a
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-09 13:48:51 +0530

    MODIFIED: added menu card route for restaurant card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,21 +1,24 @@
 import { RES_CARD_IMG } from "../utils/config";
+import { Link } from "react-router-dom";
 
 const RestaurantCard = (props) => {
   const { data } = props;
   return (
-    <div id="resCard" className="res-card">
-      <img
-        className="res-card-img"
-        src={RES_CARD_IMG + data.cloudinaryImageId}
-        alt={data.name}
-      />
-      <h1> {data.name}</h1>
-      <h2>{data.avgRating}</h2>
-      <span>
-        <h3>{data.cuisines.join(", ")}</h3>
-      </span>
-      <h4>{data.areaName}</h4>
-    </div>
+    <Link to={"/restaurant/" + data.id}>
+      <div id="resCard" className="res-card">
+        <img
+          className="res-card-img"
+          src={RES_CARD_IMG + data.cloudinaryImageId}
+          alt={data.name}
+        />
+        <h1> {data.name}</h1>
+        <h2>{data.avgRating}</h2>
+        <span>
+          <h3>{data.cuisines.join(", ")}</h3>
+        </span>
+        <h4>{data.areaName}</h4>
+      </div>
+    </Link>
   );
 };
 

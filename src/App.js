4c81b55eee968489be93cef49ab41077025bd994
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-09 11:06:36 +0530

    MODIFIED: AppLayout component added for rendering root

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import HeaderComponent from "./components/HeaderComponent";
 import RestaurantCardContainer from "./components/RestaurantCardContainer";
 
+const AppLayout = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <RestaurantCardContainer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <HeaderComponent />
-    <RestaurantCardContainer />
-  </>
-);
+root.render(<AppLayout />);

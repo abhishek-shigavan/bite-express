528b186b4b712d45581231cca072ec778e9e1d3c
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-14 12:15:44 +0530

    MODIFIED: implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import HeaderComponent from "./components/HeaderComponent";
@@ -11,10 +11,13 @@ import Help from "./components/Help";
 import SignIn from "./components/SignIn";
 import useOnlineStatus from "./utils/useOnlineStatus";
 
-const AppLayout = () => {
-  const onlineStatus = useOnlineStatus()
+const Grocery = lazy(() => import("./components/GroceryContainer"));
 
-  if (!onlineStatus) return <h1>Oops looks like you are offline..Please check your network</h1>
+const AppLayout = () => {
+  const onlineStatus = useOnlineStatus();
+
+  if (!onlineStatus)
+    return <h1>Oops looks like you are offline..Please check your network</h1>;
 
   return (
     <>
@@ -34,7 +37,15 @@ const AppRoutes = createBrowserRouter([
       { path: "/restaurant/:resId", element: <RestaurantMenuCard /> },
       { path: "/offers", element: <Offers /> },
       { path: "/cart", element: <Cart /> },
-      { path: "/help", element: <Help/> },
+      { path: "/help", element: <Help /> },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
       { path: "/signin", element: <SignIn /> },
     ],
   },

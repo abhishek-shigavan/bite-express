0fbb885a81581c1c8aa400a82f24a2452949f108
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-16 22:21:16 +0530

    MODIFIED: added img url for menu items

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
 export const HEADER_LOGO = "https://i.postimg.cc/sf4HHdtK/Screenshot-from-2023-08-05-19-39-58.png"
 export const RES_CARD_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 export const RES_MENU_CARD_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId="
+export const RES_MENU_ITEMS_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"

67762aafcd5a58a7798b013f2cefd9b77ac3a384
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-16 22:20:07 +0530

    MODIFIED: filtered menu category & its items from result

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -11,7 +11,8 @@ const useRestaurantMenu = (resId) => {
   const getResMenu = async () => {
     const resMenu = await fetch(RES_MENU_CARD_URL + resId);
     const resMenuJson = await resMenu.json();
-    setMenuItems(resMenuJson.data.cards);
+    const listItems = resMenuJson?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(item => item.card.card.itemCards || item.card.card.categories)
+    setMenuItems([...resMenuJson.data.cards, {menuListItems: listItems}]);
   };
 
   return menuItems;

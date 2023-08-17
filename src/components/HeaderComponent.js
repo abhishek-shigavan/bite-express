6460b3b7e75278f334851a717864d66f9e492cf7
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-17 15:12:21 +0530

    MODIFIED: accessing user context in header section

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,58 +1,51 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { HEADER_LOGO } from "../utils/config";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
 
 const HeaderComponent = () => {
+  const { loggedUser } = useContext(UserContext);
   const onlineStatus = useOnlineStatus();
-
   return (
     <div id="headerCnt" className="flex justify-between items-center mx-3 my-2">
       <div id="logo" className="hover:cursor-pointer w-[100px] h-[100px]">
         <img className="rounded-lg" src={HEADER_LOGO} alt="Bite Express" />
+      </div>
+      <div>
+        <span>{loggedUser}</span>
       </div>
       <div id="headerOptions">
         <ul className="flex justify-between px-2">
           <li className="px-2">{onlineStatus ? "🟢" : "🔴"}</li>
           <li className="px-2">
             <h1>
-              <Link to={"/"}>
-                Home
-              </Link>
+              <Link to={"/"}>Home</Link>
             </h1>
           </li>
           <li className="px-2">
             <h1>
-              <Link to={"/offers"}>
-                Offers
-              </Link>
+              <Link to={"/offers"}>Offers</Link>
             </h1>
           </li>
           <li className="px-2">
             <h1>
-              <Link to={"/cart"}>
-                Cart
-              </Link>
+              <Link to={"/cart"}>Cart</Link>
             </h1>
           </li>
           <li className="px-2">
             <h1>
-              <Link to={"/help"}>
-                Help
-              </Link>
+              <Link to={"/help"}>Help</Link>
             </h1>
           </li>
           <li className="px-2">
             <h1>
-              <Link to={"/grocery"}>
-                Grocery
-              </Link>
+              <Link to={"/grocery"}>Grocery</Link>
             </h1>
           </li>
           <li className="px-2">
             <h1>
-              <Link to={"/signin"}>
-                SignIn
-              </Link>
+              <Link to={"/signin"}>SignIn</Link>
             </h1>
           </li>
         </ul>

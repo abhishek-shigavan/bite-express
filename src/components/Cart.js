5177edcffbd6e912c1ebf76bf5155097336e04bd
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-19 19:57:24 +0530

    MODIFIED: displaying cart items & added method to clear cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, removeItem } from "../utils/store/cartSlice";
+
 const Cart = () => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((store) => store.cart.cartItems);
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  const handleDeleteItem = (ele) => {
+    dispatch(removeItem(ele));
+  };
+
   return (
     <>
       <h1>Cart Page</h1>
+      {cartItems.length > 0 &&
+        cartItems.map((ele) => (
+          <div
+            key={ele.card.info.id}
+            className="flex w-6/12 m-auto bg-slate-300 justify-between"
+          >
+            <li>
+              {ele.card.info.name} - {ele.card.info.price / 100}
+            </li>
+            <button onClick={() => handleDeleteItem(ele)}>Delete</button>
+          </div>
+        ))}
+      {cartItems.length > 0 && (
+        <button onClick={handleClearCart}>Clear Cart</button>
+      )}
     </>
   );
 };

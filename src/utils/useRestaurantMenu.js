505a1bda46c0d8f727e3d257ac48e18b4f0b6c62
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-19 20:01:17 +0530

    MODIFIED: updated response field

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -11,7 +11,7 @@ const useRestaurantMenu = (resId) => {
   const getResMenu = async () => {
     const resMenu = await fetch(RES_MENU_CARD_URL + resId);
     const resMenuJson = await resMenu.json();
-    const listItems = resMenuJson?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(item => item.card.card.itemCards || item.card.card.categories)
+    const listItems = resMenuJson?.data?.cards[1]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(item => item.card.card.itemCards || item.card.card.categories)
     setMenuItems([...resMenuJson.data.cards, {menuListItems: listItems}]);
   };
 

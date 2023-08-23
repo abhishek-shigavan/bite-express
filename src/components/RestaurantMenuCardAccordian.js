3aaf645131d01200b6b6f86df4c4b29283579484
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-23 11:29:45 +0530

    MODIFIED: added button container to add & update quantity of item

--- src/components/RestaurantMenuCardAccordian.js
+++ src/components/RestaurantMenuCardAccordian.js
@@ -1,7 +1,5 @@
 import { RES_MENU_ITEMS_IMG } from "../utils/config";
-import { addItemToCart } from "../utils/store/cartSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { addRestaurantInfo, clearRestaurantInfo } from "../utils/store/restaurantSlice";
+import ButtonContainer from "./ButtonContainer";
 
 const RestaurantMenuCardAccordian = (props) => {
   const { data } = props;
@@ -33,31 +31,6 @@ export default RestaurantMenuCardAccordian;
 
 export const RenderListItem = (props) => {
   const { title, listItems, showItems, setShowItems, data, resMeta } = props;
-  const dispatch = useDispatch();
-  const cartItems = useSelector((store) => store.cart.cartItems);
-  const resDetails = useSelector((store) => store.restaurant.restaurantMeta);
-
-  const handleAddNewRestaurant = (resMeta) => {
-    dispatch(clearRestaurantInfo());
-    dispatch(addRestaurantInfo(resMeta));
-  };
-
-  const handleAddToCart = (item, resMeta) => {
-    dispatch(
-      addItemToCart({
-        id: item.card.info.id,
-        name: item.card.info.name,
-        price: item.card.info.price / 100,
-        quantity: 1,
-      })
-    );
-
-    resDetails.length > 0 && resMeta.name !== resDetails[0]?.name
-      ? handleAddNewRestaurant(resMeta)
-      : resDetails.length > 0 && resMeta.name === resDetails[0]?.name
-      ? console.log("same restaurant")
-      : dispatch(addRestaurantInfo(resMeta));
-  };
 
   return (
     <>
@@ -80,26 +53,7 @@ export const RenderListItem = (props) => {
                 src={RES_MENU_ITEMS_IMG + item.card.info.imageId}
                 className="rounded-lg w-[75%] h-[96px] object-cover"
               />
-              {cartItems.length === 0 ? (
-                <button
-                  className="px-4 py-1 bg-black text-white absolute rounded-lg top-[80px] right-[22%]"
-                  onClick={() => handleAddToCart(item, resMeta)}
-                >
-                  Add
-                </button>
-              ) : (
-                <div className="flex absolute top-[80px] right-[15%]">
-                  <button className="border border-gray-400 rounded-lg px-3 py-1 bg-black text-white">
-                    -
-                  </button>
-                  <span className="border border-gray-400 rounded-lg px-3 py-1 bg-black text-white">
-                    {cartItems.length}
-                  </span>
-                  <button className="border border-gray-400 rounded-lg px-3 py-1 bg-black text-white">
-                    +
-                  </button>
-                </div>
-              )}
+              <ButtonContainer menuItemName={item.card.info.name} item={item} resMeta={resMeta}/>
             </div>
           </div>
         ))}

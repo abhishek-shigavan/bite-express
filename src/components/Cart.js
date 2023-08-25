ad2ceaa0318f6c4c949505f759f665fa440e706b
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-25 11:18:30 +0530

    MODIFIED: displaying restaurant details & added quantity control in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart, removeItem } from "../utils/store/cartSlice";
 import { clearRestaurantInfo } from "../utils/store/restaurantSlice";
+import { RES_CARD_IMG } from "../utils/config";
+import ButtonContainer from "./ButtonContainer";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const cartItems = useSelector((store) => store.cart.cartItems);
   const resDetails = useSelector((store) => store.restaurant.restaurantMeta);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const total = cartItems.reduce(
+      (prev, curr) => prev + curr.price * curr.quantity,
+      0
+    );
+    setTotalPrice(total);
+  }, [cartItems]);
 
   const handleRemoveItem = (index) => {
     if (cartItems.length === 1) {
@@ -22,25 +34,49 @@ const Cart = () => {
   };
 
   return (
-    <>
-      <h1>{resDetails[0].name}</h1>
-      <h1>Cart Page</h1>
-      {cartItems.length > 0 &&
-        cartItems.map((ele, index) => (
-          <div
-            key={ele.id}
-            className="flex w-6/12 m-auto bg-slate-300 justify-between"
-          >
-            <li>
-              {ele.name} - {ele.price} - {ele.quantity}
-            </li>
-            <button onClick={() => handleRemoveItem(index)}>Delete</button>
+    <div className="min-h-screen min-w-full bg-[#E9ECEE] absolute">
+      {resDetails.length > 0 ? (
+        <div className="flex flex-col w-6/12 bg-white mx-auto mt-20 p-6">
+          <div className="flex pb-9">
+            <img
+              src={RES_CARD_IMG + resDetails[0].imgId}
+              className="w-4/12 rounded-xl mr-5"
+            />
+            <div className="flex flex-col">
+              <span className="font-extrabold text-2xl">
+                {resDetails[0].name}
+              </span>
+              <span className="text-[#686B78]">{resDetails[0].area}</span>
+            </div>
           </div>
-        ))}
-      {cartItems.length > 0 && (
-        <button onClick={() => handleClearCart()}>Clear Cart</button>
+          <div>
+            {cartItems.map((ele, index) => (
+              <div key={ele.id} className="flex justify-between items-center">
+                <div className="flex flex-col w-6/12">
+                  <span className="font-bold text-lg">{ele.name}</span>
+                  <span className="text-[#686B78]">₹ {ele.price}</span>
+                </div>
+                <div className="w-3/12">
+                <ButtonContainer menuItemName={ele.name} resMeta={resDetails[0]} position={true}/></div>
+                <span className="text-[#686B78] w-2/12">
+                  ₹ {ele.price * ele.quantity}
+                </span>
+                <div className="flex justify-end w-1/12">
+                <button onClick={() => handleRemoveItem(index)}>Delete</button></div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div></div>
       )}
-    </>
+      {cartItems.length > 0 && (
+        <>
+          <span>{totalPrice}</span>
+          <button onClick={() => handleClearCart()}>Clear Cart</button>
+        </>
+      )}
+    </div>
   );
 };
 

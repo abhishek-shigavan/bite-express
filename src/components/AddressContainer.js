d4f8dbe2605f5fc1c42d8e76153a2946c4bed5e3
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-08-26 23:17:28 +0530

    MODIFIED: added edit functionality for address

--- src/components/AddressContainer.js
+++ src/components/AddressContainer.js
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addAddressDetails, removeAddressDetails } from "../utils/store/userSlice";
 
 const AddressContainer = () => {
   const [openModal, setOpenModal] = useState(false);
+  const [enableUpdate, setEnableUpdate] = useState(false);
   const [addressDetails, setAddressDetails] = useState({});
-  const userAddress = useSelector((store) => store.user.addressDetails) 
-  const dispatch = useDispatch()
+  const userAddress = useSelector((store) => store.user.addressDetails);
+  const dispatch = useDispatch();
+
+  const handleEditAddress = (item) => {
+    setEnableUpdate(true);
+    setAddressDetails(item);
+    setOpenModal(true);
+  };
 
   const resetAddressDetails = (action = "none") => {
-    if(action === "save") dispatch(addAddressDetails(addressDetails)) 
-    setOpenModal(false)
-    setAddressDetails({ apartment: '', landmark: '', area: '', type: '' })
-  }
+    if (action === "save") dispatch(addAddressDetails(addressDetails));
+    setOpenModal(false);
+    setAddressDetails({ apartment: "", landmark: "", area: "", type: "" });
+  };
 
   return (
     <div className="flex flex-col w-6/12 bg-white mx-auto my-10 p-6">
@@ -25,21 +32,35 @@ const AddressContainer = () => {
           Add
         </button>
       </div>
-      
-      {userAddress.map((item, index) => 
+
+      {userAddress.map((item, index) => (
         <div key={index} className="flex border rounded-lg p-4 my-4">
           <div className="flex flex-col w-8/12">
-          <span>{item.type}</span>
-          <span>{item.apartment}</span>
-          <span>{item.landmark}</span>
-          <span>{item.area}</span>
+            <span>{item.type}</span>
+            <span>{item.apartment}</span>
+            <span>{item.landmark}</span>
+            <span>{item.area}</span>
           </div>
           <div className="flex justify-center w-4/12">
-          <div><button className="border rounded-lg px-3 py-1 mr-4">Edit</button></div>
-          <div><button onClick={() => dispatch(removeAddressDetails(index))} className="border rounded-lg px-3 py-1">Delete</button></div>
+            <div>
+              <button
+                onClick={() => handleEditAddress(item)}
+                className="border rounded-lg px-3 py-1 mr-4"
+              >
+                Edit
+              </button>
+            </div>
+            <div>
+              <button
+                onClick={() => dispatch(removeAddressDetails(index))}
+                className="border rounded-lg px-3 py-1"
+              >
+                Delete
+              </button>
+            </div>
           </div>
         </div>
-      )}
+      ))}
 
       <div
         className={
@@ -97,27 +118,44 @@ const AddressContainer = () => {
                 });
               }}
             />
-          </div>          
+          </div>
           <div className="flex justify-between py-3">
-            <button className="border border-[#686B78] rounded-lg px-3 py-2"
-              onClick={() => setAddressDetails({ ...addressDetails, type: "Home" })}
+            <button
+              className="border border-[#686B78] rounded-lg px-3 py-2"
+              onClick={() =>
+                setAddressDetails({ ...addressDetails, type: "Home" })
+              }
             >
               Home
             </button>
-            <button className="border border-[#686B78] rounded-lg px-3 py-2"
-              onClick={() => setAddressDetails({ ...addressDetails, type: "Work" })}
+            <button
+              className="border border-[#686B78] rounded-lg px-3 py-2"
+              onClick={() =>
+                setAddressDetails({ ...addressDetails, type: "Work" })
+              }
             >
               Work
             </button>
-            <button className="border border-[#686B78] rounded-lg px-3 py-2"
-              onClick={() => setAddressDetails({ ...addressDetails, type: "Other" })}
+            <button
+              className="border border-[#686B78] rounded-lg px-3 py-2"
+              onClick={() =>
+                setAddressDetails({ ...addressDetails, type: "Other" })
+              }
             >
               Other
             </button>
           </div>
           <div>
-          <button className="border border-[#686B78] rounded-lg px-3 py-2"
-            onClick={() => resetAddressDetails("save")}>Save</button>
+            {enableUpdate ? (
+              <button className="border rounded-lg px-3 py-2">Update</button>
+            ) : (
+              <button
+                className="border border-[#686B78] rounded-lg px-3 py-2"
+                onClick={() => resetAddressDetails("save")}
+              >
+                Save
+              </button>
+            )}
           </div>
         </div>
       </div>
